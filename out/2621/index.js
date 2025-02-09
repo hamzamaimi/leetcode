ee7b4bbfadfabb519aa181ffac56ac71f3a6425f
hamzamaimi <hamzamaimi0901@gmail.com>
2025-02-09 11:41:26 +0100

    exercise 2621 done

---
+++ out/2621/index.js
@@ -0,0 +1,20 @@
+// Given a positive integer millis, 
+// write an asynchronous function that sleeps for millis milliseconds. 
+// It can resolve any value.
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function sleep(millis) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve) => { setTimeout(resolve, millis); });
+    });
+}
+let t = Date.now();
+sleep(100).then(() => console.log(Date.now() - t)); // 100
+//# sourceMappingURL=index.js.map
