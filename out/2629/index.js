b4ec19491e5878079b72c2d159918fdb88ff1596
hamzamaimi <hamzamaimi0901@gmail.com>
2025-03-18 21:47:58 +0100

    Excercise 2703 Done

--- out/2629/index.js
+++ out/2629/index.js
@@ -1,23 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// function compose(functions: F[]): F {
-//     return function(x) {
-//       let accumulator = x;
-//       for(let i = functions.length - 1; i >= 0; i++){
-//         accumulator = functions[i](accumulator)
-//       }
-//       return accumulator;
-//     }
-// };
 function compose(functions) {
     return function (x) {
-        let result = x;
+        let accumulator = x;
         for (let i = functions.length - 1; i >= 0; i--) {
-            result = functions[i](result);
+            accumulator = functions[i](accumulator);
         }
-        return result;
+        return accumulator;
     };
 }
+;
 const fn = compose([x => x + 1, x => 2 * x]);
 console.log(fn(4)); // 9
 //# sourceMappingURL=index.js.map
---
+++ out/2703/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function argumentsLength(...args) {
+    return args.length;
+}
+;
+console.time('as');
+argumentsLength(1, 2, 3);
+console.timeEnd('as');
+//# sourceMappingURL=index.js.map
